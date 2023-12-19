32ad042fbd453973031c36ed0927e78b60d084df
Subhajit Gorai <subhajit.gorai@sayf.in>
2023-12-19 18:16:59 +0530

    apply to jobs and fetch job applicants

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { JobsService } from './jobs.service';
 import { CreateJobDto } from './dto/create-job.dto';
 import { UpdateJobDto } from './dto/update-job.dto';
+import { Candidate } from 'src/candidate/schema/candidate.schema';
 
 @Controller('jobs')
 export class JobsController {
@@ -17,9 +26,14 @@ export class JobsController {
     return this.jobsService.findAll();
   }
 
-  @Get(':id')
+  @Post('apply/:id')
+  applyToJob(@Param('id') id: string, @Body() candidate: Candidate) {
+    return this.jobsService.applyToJob(id, candidate);
+  }
+
+  @Get('applicants/:id')
   findOne(@Param('id') id: string) {
-    return this.jobsService.findOne(+id);
+    return this.jobsService.getApplicants(id);
   }
 
   @Patch(':id')
--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateJobDto } from './dto/create-job.dto';
 import { UpdateJobDto } from './dto/update-job.dto';
 import { Jobs } from './schema/jobs.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Candidate } from 'src/candidate/schema/candidate.schema';
 
 @Injectable()
 export class JobsService {
@@ -17,12 +18,44 @@ export class JobsService {
     return createdJobProvider.save();
   }
 
-  findAll() {
+  async findAll(): Promise<Jobs[]> {
     return this.jobsModel.find().exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} job`;
+  async getApplicants(jobId: string): Promise<any> {
+    try {
+      const job = await this.jobsModel.findById(jobId).exec();
+
+      if (!job) {
+        throw new NotFoundException(`Job with id ${jobId} not found`);
+      }
+
+      const applicants = job.applicants; // Assuming 'applicants' is the array field in your schema
+
+      return applicants;
+    } catch (error) {
+      throw new NotFoundException(`Job with id ${jobId} not found`);
+    }
+  }
+
+  async applyToJob(jobId: string, candidate: Candidate): Promise<any> {
+    try {
+      const job = await this.jobsModel.findById(jobId).exec();
+
+      if (!job) {
+        throw new NotFoundException(`Job with id ${jobId} not found`);
+      }
+
+      // Add the candidate to the 'applicants' array
+      job.applicants.push(candidate);
+
+      // Save the updated job document
+      await job.save();
+
+      return job.applicants; // Return the updated list of applicants
+    } catch (error) {
+      throw new NotFoundException(`Job with id ${jobId} not found`);
+    }
   }
 
   update(id: number, updateJobDto: UpdateJobDto) {
